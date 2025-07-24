1874e30017032919d498b91042708ea842dbc0ad
Jester Rey Perez <165446384+jesterreyperez@users.noreply.github.com>
2025-07-24 10:36:47 +0800

    Use existing UserRepository and add TransactionRecordRepository

---
+++ src/main/java/com/jpmc/midascore/repository/TransactionRecordRepository.java
@@ -0,0 +1,8 @@
+package com.jpmc.midascore.repository;
+
+import com.jpmc.midascore.model.TransactionRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRecordRepository extends JpaRepository<TransactionRecord, Long> {
+    // JPA gives us basic save, find, delete methods automatically
+}
